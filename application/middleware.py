4dd5297618bdfb3b6fe6f1154e4de6f7095b588c
artur1214 <artur.2002.artur@gmail.com>
2022-05-17 17:57:44 +0600

    Небольшие исправления тестов

--- application/middleware.py
+++ application/middleware.py
@@ -1,16 +1,13 @@
-from rest_framework.authentication import BaseAuthentication
 from rest_framework_simplejwt.authentication import JWTAuthentication
-#from social_auth.backends.contrib.vk import VKOAuth2Backend, VKOpenAPIAuth
-from rest_framework_simplejwt.exceptions import AuthenticationFailed, \
-    InvalidToken
+from rest_framework_simplejwt.exceptions import AuthenticationFailed
 from django.utils.translation import gettext_lazy as _
-from rest_framework_simplejwt.settings import api_settings
 from rest_framework_simplejwt.tokens import AccessToken
 
 
 class SocialAuth(JWTAuthentication):
     def __init__(self, *args, **kwargs):
         super(SocialAuth, self).__init__(*args, **kwargs)
+
     def authenticate(self, request):
         header = self.get_header(request)
         if header is None:
@@ -20,11 +17,8 @@ class SocialAuth(JWTAuthentication):
         if raw_token is None:
             return None
         validated_token = self.get_validated_token(raw_token)
-        #print(validated_token)
         user = self.get_user(validated_token)
-        #print(user)
         if user:
-            #print(11123, user)
             return user, validated_token
         else:
             return None
@@ -33,14 +27,10 @@ class SocialAuth(JWTAuthentication):
         """
         Attempts to find and return a user using the given validated token.
         """
-        #print(validated_token)
-        #print(type(validated_token))
         try:
             user_id = validated_token['vk_id']
-            #print(111, user_id)
         except KeyError:
             return None
-            #raise AuthenticationFailed(_('Token contained no recognizable user identification'))
         try:
             user = self.user_model.objects.get(**{'vk_id': user_id})
 
--- tests/medicine_test.py
+++ tests/medicine_test.py
@@ -1,19 +1,18 @@
-from django.contrib.auth.models import User
+from application.models import User
 from django.db.models import QuerySet
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework.response import Response
 
-from application.management.factories.medicine import MedicineFactory
 from application.models import Medicine, TypeChoices
-from django.test import Client
+
 
 class MedicineTestCase(TestCase):
     databases = ['default']
 
     def setUp(self):
         User.objects.create_user('artur1214', '', 'wasd123')
-        #MedicineFactory.create()
+        # MedicineFactory.create()
         s = Medicine.objects.create(
             type=TypeChoices.DRUG,
             name='Фентанил',
@@ -21,19 +20,18 @@ class MedicineTestCase(TestCase):
             price=1000
         )
         Medicine.objects.create(type=TypeChoices.PAINKILLER, name='Некст',
-            model='ООО Фортелар', price=160)
+                                model='ООО Фортелар', price=160)
 
         Medicine.objects.create(type=TypeChoices.TABS, name='Нурофен',
-            model='ООО НУрафен', price=70)
+                                model='ООО НУрафен', price=70)
         token_url = reverse('token_obtain_pair')
         jwt_fetch_data = {'username': 'artur1214', 'password': 'wasd123'}
         response = self.client.post(token_url, jwt_fetch_data, format='json')
         token = response.data['access']
         self.client.defaults.update({'HTTP_AUTHORIZATION': f'Bearer {token}'})
-        #self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
-
+        # self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
         self.data = {'type': 1, 'name': 'Тестовое лекарство', 'model': 'ООО ТестКонтора',
-        'price': 5000}
+                     'price': 5000}
 
     def test_get_medicine(self):
         resp: Response = self.client.get('/api/medicine/')
@@ -43,19 +41,18 @@ class MedicineTestCase(TestCase):
         self.assertEqual(len(resp), Medicine.objects.count())
 
     def test_create_medicine(self):
-
         resp: Response = self.client.post('/api/medicine/', data=self.data)
         self.assertEqual(resp.status_code, 201)
         created_obj = Medicine.objects.get(**self.data)
         self.assertIsInstance(created_obj, Medicine)
 
-
     def test_update_medicine(self):
         data = {'name': 'ещё более тестовое лекарство', 'price': 300}
         self.test_create_medicine()
-        resp: Response = self.client.patch(f'/api/medicine/{Medicine.objects.get(**self.data).pk}/', data=data, content_type='application/json')
+        resp: Response = self.client.patch(f'/api/medicine/{Medicine.objects.get(**self.data).pk}/', data=data,
+                                           content_type='application/json')
         self.assertEqual(resp.status_code, 200)
-        #print(resp.data)
+        # print(resp.data)
         med_obj = Medicine.objects.filter(**data)
         self.assertIsInstance(med_obj, (Medicine, QuerySet))
         self.assertEqual(med_obj[0].name, data['name'])
